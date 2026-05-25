f32933ea6f2281cd7ea0339ed415ba61c5af1bfe
nianxoxo <nianatasha444@gmail.com>
2026-05-25 19:15:30 -0400

    Completed Lab 2 user management

--- server/frontend/src/components/Header/Header.jsx
+++ server/frontend/src/components/Header/Header.jsx
@@ -13,13 +13,12 @@ const Header = () => {
     const json = await res.json();
     if (json) {
       let username = sessionStorage.getItem('username');
+      alert("Logging out " + username + "...");
       sessionStorage.removeItem('username');
       window.location.href = window.location.origin;
-      window.location.reload();
-      alert("Logging out "+username+"...")
     }
     else {
-      alert("The user could not be logged out.")
+      alert("The user could not be logged out.");
     }
   };
     
@@ -32,32 +31,32 @@ let curr_user = sessionStorage.getItem('username')
 //If the user is logged in, show the username and logout option on home page
 if ( curr_user !== null &&  curr_user !== "") {
     home_page_items = <div className="input_panel">
-      <text className='username'>{sessionStorage.getItem("username")}</text>
-    <a className="nav_item" href="/djangoapp/logout" onClick={logout}>Logout</a>
+      <span className='username' style={{paddingRight: "10px", fontWeight: "bold"}}>{sessionStorage.getItem("username")}</span>
+    <a className="nav_item" href="/" onClick={logout}>Logout</a>
   </div>
 }
     return (
         <div>
-          <nav class="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkturquoise",height:"1in"}}>
-            <div class="container-fluid">
+          <nav className="navbar navbar-expand-lg navbar-light" style={{backgroundColor:"darkturquoise",height:"1in"}}>
+            <div className="container-fluid">
               <h2 style={{paddingRight: "5%"}}>Dealerships</h2>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" style={{fontSize: "larger"}} aria-current="page" href="/">Home</a>
+              <div className="collapse navbar-collapse" id="navbarText">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" style={{fontSize: "larger"}} aria-current="page" href="/">Home</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/about">About Us</a>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{fontSize: "larger"}} href="/about">About Us</a>
                   </li>
-                  <li class="nav-item">
-                    <a class="nav-link" style={{fontSize: "larger"}} href="/contact">Contact Us</a>
+                  <li className="nav-item">
+                    <a className="nav-link" style={{fontSize: "larger"}} href="/contact">Contact Us</a>
                   </li>
                 </ul>
-                <span class="navbar-text">
-                  <div class="loginlink" id="loginlogout">
+                <span className="navbar-text">
+                  <div className="loginlink" id="loginlogout">
                   {home_page_items}
                   </div>
                   </span>
@@ -68,4 +67,4 @@ if ( curr_user !== null &&  curr_user !== "") {
     )
 }
 
-export default Header
+export default Header;
